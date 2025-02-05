ff7f69209687857693fefd463b7ada0645d4f254
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2025-02-05 15:29:11 +0700

    DbContext и его подключение

---
+++ Catalog_Common/SD.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog_Common
+{
+    public static class SD
+    {
+
+        public enum DbConnectionMode
+        {
+            SqlLight,
+            PostgreSQL,
+            MSSQL
+        }
+
+        public static int SqlCommandConnectionTimeout = 180;
+    }
+}
--- Catalog_WebAPI/Program.cs
+++ Catalog_WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using Catalog_Common;
+using Catalog_DataAccess;
+using Microsoft.EntityFrameworkCore;
+using static Catalog_Common.SD;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,45 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+
+var dbConnectionMode = builder.Configuration.GetValue<string>("DbConnectionMode");
+
+DbConnectionMode dbConnectionModeEnum = (DbConnectionMode)Enum.Parse(typeof(DbConnectionMode), dbConnectionMode, true);
+
+switch (dbConnectionModeEnum)
+{
+    case DbConnectionMode.MSSQL:
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogDBMSSQLConnection"),
+                    u => u.CommandTimeout(SD.SqlCommandConnectionTimeout))
+                );
+            break;
+        }
+    case DbConnectionMode.PostgreSQL:
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("CatalogDBPostgresSQLConnection"),
+                u => u.CommandTimeout(SD.SqlCommandConnectionTimeout))
+            );
+            break;
+        }
+    case DbConnectionMode.SqlLight:
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("CatalogDBSqlLightConnection"),
+                u => u.CommandTimeout(SD.SqlCommandConnectionTimeout))
+            );
+            break;
+        }
+    default:
+        {
+            break;
+        }
+}
+
 
 var app = builder.Build();
 
