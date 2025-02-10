2b2e07868139e0d42adbd707a334987a2e6d9a46
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2025-02-11 02:49:20 +0700

    Добавлена документация к действиям контроллера StatesController и методам репозитория StateRepository

--- Catalog_WebAPI/Controllers/StatesController.cs
+++ Catalog_WebAPI/Controllers/StatesController.cs
@@ -37,21 +37,37 @@ namespace Catalog_WebAPI.Controllers
             return Ok(_mapper.Map<IEnumerable<State>, IEnumerable<StateItemResponse>>(gotStates));
         }
 
+        /// <summary>
+        /// Получить список всех статусов экземпляров книг находящихся в архиве
+        /// </summary>
+        /// <returns>Возвращает список всех статусов находящихся в архиве - объектов типа StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение</response>
         [HttpGet("AllInArchive")]
+        [ProducesResponseType(typeof(List<StateItemResponse>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<List<StateItemResponse>>> GetAllStatesInArchiveAsync()
         {
             var gotStates = await GetStatesAsync(SD.GetAllItems.ArchiveOnly);
             return Ok(_mapper.Map<IEnumerable<State>, IEnumerable<StateItemResponse>>(gotStates));
         }
 
+        /// <summary>
+        /// Получить список всех статусов экземпляров книг находящихся НЕ в архиве
+        /// </summary>
+        /// <returns>Возвращает список всех статусов находящихся НЕ в архиве - объектов типа StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение</response>
         [HttpGet("AllNotInArchive")]
+        [ProducesResponseType(typeof(List<StateItemResponse>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<List<StateItemResponse>>> GetAllStatesNotInArchiveAsync()
         {
             var gotStates = await GetStatesAsync(SD.GetAllItems.NotArchiveOnly);
             return Ok(_mapper.Map<IEnumerable<State>, IEnumerable<StateItemResponse>>(gotStates));
-            return Ok();
         }
 
+        /// <summary>
+        /// Вспомогательный метод возвращающий список всех статусов, статусов не в архиве или статусов в архиве в зависимости от входного параметра
+        /// </summary>
+        /// <param name="getAllItems">Параметр указывает возвращать все статусы, только статусы в архиве или только статусы не в архиве</param>
+        /// <returns>Возвращает список статусов  - объектов типа State</returns>
         [NonAction]
         public async Task<IEnumerable<State>> GetStatesAsync(SD.GetAllItems getAllItems = SD.GetAllItems.All)
         {
@@ -77,9 +93,17 @@ namespace Catalog_WebAPI.Controllers
             return gotStates;
         }
 
-
+        /// <summary>
+        /// Получить чтатус по ИД
+        /// </summary>
+        /// <param name="id">ИД статуса</param>
+        /// <returns>Возвращает найденый по ИД статус - объект типа StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение</response>
+        /// <response code="404">Статус с заданным Id не найден</response>
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<StateItemResponse>> GetSateByIdAsync(int id)
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<StateItemResponse>> GetStateByIdAsync(int id)
         {
             var state = await _stateRepository.GetByIdAsync(id);
 
@@ -89,8 +113,17 @@ namespace Catalog_WebAPI.Controllers
             return Ok(_mapper.Map<State, StateItemResponse>(state));
         }
 
+        /// <summary>
+        /// Создание нового статуса
+        /// </summary>
+        /// <param name="request">Параметры создаваемого статуса - объект типа StateItemCreateRequest</param>
+        /// <returns>Возвращает созданый статус - объект типа StateItemResponse</returns>
+        /// <response code="201">Успешное выполнение. Статус создан</response>
+        /// <response code="400">Не удалось добавить статус. Причина описана в ответе</response>  
         [HttpPost]
-        public async Task<ActionResult<int>> CreateStateAsync(StateItemCreateRequest request)
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<StateItemResponse>> CreateStateAsync(StateItemCreateRequest request)
         {
             if (request.IsInitialState)
             {
@@ -119,10 +152,22 @@ namespace Catalog_WebAPI.Controllers
                 routVar = new UriBuilder(Request.Scheme, Request.Host.Host, (int)Request.Host.Port, Request.Path.Value).ToString()
                     + "/" + addedState.Id.ToString();
             }
-            return Created(routVar, addedState.Id);
+            return Created(routVar, _mapper.Map<State, StateItemResponse>(addedState));
         }
 
+        /// <summary>
+        /// Изменение существующего статуса
+        /// </summary>
+        /// <param name="id">ИД изменяемого статуса</param>
+        /// <param name="request">Новые данные изменяемого статуса - объект типа StateItemUpdateRequest</param>
+        /// <returns>Возвращает данные изменённого статуса - объект StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение. Статус изменён</response>
+        /// <response code="400">Не удалось изменить статус. Причина описана в ответе</response>  
+        /// <response code="404">Не удалось найти статус с указаным ИД</response>  
         [HttpPut("{id:int}")]
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<StateItemResponse>> EditStateAsync(int id, StateItemUpdateRequest request)
         {
             var foundState = await _stateRepository.GetByIdAsync(id);
@@ -145,7 +190,20 @@ namespace Catalog_WebAPI.Controllers
             return Ok(_mapper.Map<State, StateItemResponse>(updatedState));
         }
 
+
+
+        /// <summary>
+        /// Удалить статус с указаным ИД в архив
+        /// </summary>
+        /// <param name="id">ИД удаляемого в архив статуса</param>
+        /// <returns>Возвращает данные удалённого в архив статуса - объект StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение. Статус удалён в архив</response>
+        /// <response code="400">Не удалось удалить статус в архив, так как статус уже в архиве</response>  
+        /// <response code="404">Не удалось найти статус с указаным ИД</response>  
         [HttpPut("DeleteToArchive/{id:int}")]
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<StateItemResponse>> DeleteStateToArchiveAsync(int id)
         {
             var foundState = await _stateRepository.GetByIdAsync(id);
@@ -158,7 +216,19 @@ namespace Catalog_WebAPI.Controllers
             return Ok(_mapper.Map<State, StateItemResponse>(updatedState));
         }
 
+
+        /// <summary>
+        /// Восстановить статус с указаным ИД из архива
+        /// </summary>
+        /// <param name="id">ИД восстанавливаемого из архива статуса</param>
+        /// <returns>Возвращает данные восстановленного из архива статуса - объект StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение. Статус восстановлен из архива архив</response>
+        /// <response code="400">Не удалось восстановить статус из архива, так как статус уже находится не в архиве</response>  
+        /// <response code="404">Не удалось найти статус с указаным ИД</response>  
         [HttpPut("RestoreFromArchive/{id:int}")]
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<StateItemResponse>> RestoreStateFromArchiveAsync(int id)
         {
             var foundState = await _stateRepository.GetByIdAsync(id);
@@ -170,7 +240,19 @@ namespace Catalog_WebAPI.Controllers
             return Ok(await _stateRepository.UpdateAsync(foundState));
         }
 
+
+        /// <summary>
+        /// Утановить статус как статус по умолчанию для новых экземпляров книг
+        /// </summary>
+        /// <param name="id">ИД статуса</param>
+        /// <returns>Возвращает данные статуса, установленного в качестве статуса по умолчанию для новых экземпляров книг - объект StateItemResponse</returns>
+        /// <response code="200">Успешное выполнение. Статус установлен в качестве статуса по умолчанию для новых экземпляров книг</response>
+        /// <response code="400">Не удалось выполнить операцию, так как статус уже является статусом по умолчанию для новых экземпляров книг</response>  
+        /// <response code="404">Не удалось найти статус с указаным ИД</response>  
         [HttpPut("SetIsInitialState/{id:int}")]
+        [ProducesResponseType(typeof(StateItemResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<StateItemResponse>> SetIsInitialStateById(int id)
         {
             var foundState = await _stateRepository.GetByIdAsync(id);
